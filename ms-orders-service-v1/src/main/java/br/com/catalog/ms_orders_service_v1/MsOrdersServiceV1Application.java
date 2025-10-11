67c5ac2732d514b764ab5e75e4942d51ab0d0200
Matheuslemes <matheuslemesmsl@gmail.com>
2025-10-11 14:55:17 -0300

    orbity: feat — ms-orders: criação inicial do ms-orders.

---
+++ ms-orders-service-v1/src/main/java/br/com/catalog/ms_orders_service_v1/MsOrdersServiceV1Application.java
@@ -0,0 +1,13 @@
+package br.com.catalog.ms_orders_service_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrdersServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrdersServiceV1Application.class, args);
+	}
+
+}
