62a0b150c5116ebe827e27925eda4db6629393a6
Matheuslemes <matheuslemesmsl@gmail.com>
2025-10-22 17:22:46 -0300

    orbity: feat — ms-cart, api-gateway: criação de ms-cart e api-gateway.

---
+++ ms-cart-service-v1/src/main/java/br/com/orbity/ms_cart_service_v1/MsCartServiceV1Application.java
@@ -0,0 +1,13 @@
+package br.com.orbity.ms_cart_service_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCartServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCartServiceV1Application.class, args);
+	}
+
+}
---
+++ ms-cart-service-v1/src/test/java/br/com/orbity/ms_cart_service_v1/MsCartServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orbity.ms_cart_service_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCartServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
